dd1894998d35bd93d0bcd1ece96bfb380c31333e
Dong Yun Lee <dongyun909@gmail.com>
2024-04-01 15:57:16 +0900

    revision240401

--- extras/CodeToRun.R
+++ extras/CodeToRun.R
@@ -1,4 +1,4 @@
-library(glpHiraV1)
+library(glp1HIRAV2)
 
 # Optional: specify where the temporary files (used by the Andromeda package) will be created:
 options(andromedaTempFolder = "s:/andromedaTemp")
@@ -7,7 +7,7 @@ options(andromedaTempFolder = "s:/andromedaTemp")
 maxCores <- parallel::detectCores()
 
 # The folder where the study intermediate and result files will be written:
-outputFolder <- "s:/glpHiraV1"
+outputFolder <- "s:/glp1HIRAV2"
 
 # Details for connecting to the server:
 connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -1,6 +1,6 @@
 # Copyright 2020 Observational Health Data Sciences and Informatics
 #
-# This file is part of glpHiraV1
+# This file is part of glp1HIRAV2
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,13 +16,13 @@
 
 # Format and check code ---------------------------------------------------
 OhdsiRTools::formatRFolder()
-OhdsiRTools::checkUsagePackage("glpHiraV1")
+OhdsiRTools::checkUsagePackage("glp1HIRAV2")
 OhdsiRTools::updateCopyrightYearFolder()
 devtools::spell_check()
 
 # Create manual -----------------------------------------------------------
-unlink("extras/glpHiraV1.pdf")
-shell("R CMD Rd2pdf ./ --output=extras/glpHiraV1.pdf")
+unlink("extras/glp1HIRAV2.pdf")
+shell("R CMD Rd2pdf ./ --output=extras/glp1HIRAV2.pdf")
 
 # Create vignettes ---------------------------------------------------------
 rmarkdown::render("vignettes/UsingSkeletonPackage.Rmd",
@@ -43,7 +43,7 @@ ROhdsiWebApi::insertCohortDefinitionSetInPackage(fileName = "CohortsToCreate.csv
                                                  insertTableSql = TRUE,
                                                  insertCohortCreationR = TRUE,
                                                  generateStats = FALSE,
-                                                 packageName = "glpHiraV1")
+                                                 packageName = "glp1HIRAV2")
 
 # Create analysis details -------------------------------------------------
 source("extras/CreateStudyAnalysisDetails.R")
@@ -51,4 +51,4 @@ createAnalysesDetails("inst/settings/")
 createPositiveControlSynthesisArgs("inst/settings/")
 
 # Store environment in which the study was executed -----------------------
-OhdsiRTools::createRenvLockFile("glpHiraV1")
+OhdsiRTools::createRenvLockFile("glp1HIRAV2")
